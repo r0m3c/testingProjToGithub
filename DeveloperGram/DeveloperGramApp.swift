c7e1756e2abaa3102f671a52c1b9a16521995bd3
Leandro Gamarra <leandrogamarra@rocketmail.com>
2021-10-28 16:36:01 -0400

    Initial Commit

---
+++ DeveloperGram/DeveloperGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeveloperGramApp.swift
+//  DeveloperGram
+//
+//  Created by Leandro Gamarra on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct DeveloperGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
